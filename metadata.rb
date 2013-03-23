7e8fdc2476b1e261d35c2dff57a7e4b3c4c444a6
Tima Maslyuchenko <insside@gmail.com>
2013-03-23 20:14:39 +0200

    added metadata

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'application_puma'
+description      'Chef cookbook for puma application'
+maintainer       'Tima Maslyuchenko'
+maintainer_email 'insside@gmail.com'
+
+depends          'puma'
+
+version          '0.0.1'
