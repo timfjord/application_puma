c0cfa931f9fc430abd58f7db478dbe934cdc7a13
Tima Maslyuchenko <insside@gmail.com>
2013-03-24 12:33:56 +0200

    make sure adding monit config running in correct run_context

--- providers/puma.rb
+++ providers/puma.rb
@@ -1,30 +1,6 @@
 include Chef::Mixin::LanguageIncludeRecipe
 
 action :before_compile do
-  include_recipe 'puma'
-  
-  new_resource = @new_resource
-  
-  current_path = ::File.join(new_resource.path, 'current')
-  config_path = ::File.join(new_resource.path, 'shared', 'config', 'puma.rb')
-  pidfile_path = ::File.join(new_resource.path, 'shared', 'tmp', 'pids', 'puma.pid')
-  state_param_path = ::File.join(new_resource.path, 'shared', 'tmp', 'pids', 'puma.state')
-  puma_config config_path do
-    pidfile pidfile_path
-    state_path state_param_path
-    bind new_resource.bind
-    owner new_resource.user
-  end
-  
-  puma_app "#{current_path}" do
-    action :remove
-  end
-  
-  puma_app "#{current_path}" do
-    user new_resource.user
-    config config_path
-    log ::File.join(new_resource.path, 'shared', 'log', 'puma.log')
-  end
 end
 
 action :before_deploy do
@@ -37,7 +13,36 @@ action :before_symlink do
 end
 
 action :before_restart do
+  include_recipe 'puma'
+  
+  new_resource = @new_resource
+  
+  current_path = ::File.join(new_resource.path, 'current')
+  config_path = ::File.join(new_resource.path, 'shared', 'config', 'puma.rb')
+  pidfile_path = ::File.join(new_resource.path, 'shared', 'tmp', 'pids', 'puma.pid')
+  puma_config config_path do
+    pidfile pidfile_path
+    state_path state_param_path
+    bind new_resource.bind
+    owner new_resource.user
+  end
+  
+  with_context new_resource.application_provider.run_context do
+    puma_monit new_resource.name do
+      path current_path
+      pidfile pidfile_path
+      config config_path
+      user new_resource.user
+    end
+  end
 end
 
 action :after_restart do
+end
+
+def with_context(context)
+  saved_run_context = @run_context
+  @run_context = context
+  yield
+  @run_context = saved_run_context
 end
